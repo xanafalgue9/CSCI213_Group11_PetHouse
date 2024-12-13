42fbd5fd687e62a696d9c5555219d0dcf5c9045c
noahw <noahw@NOAH-JOHNSON>
2024-12-12 22:29:52 -0600

    Added user getter and setters

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace PetHouse.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetHouse.Models
 {
     /*
      * STOPGAP CLASS
@@ -6,5 +8,51 @@
     public class User
     {
         public int Id { get; set; }
+       
+        //We might need this?
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        [Phone]
+        public string? Phone { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 4)]
+        public string? Username { get; set; }
+
+        [Required]
+        [StringLength(24, MinimumLength = 3)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(40, MinimumLength = 4)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        public string? Location { get; set; }
+
+        [Required]
+        [Range(0, 1000000)] //$0 - $1,000,000
+        public double? AnnualIncome{ get; set; }
+
+        [Required]
+        [Range(0,3)]   // 0-3 pets they already have
+        public int? NumPreownedPets { get; set; }
+
+        
+
+        /*
+         * Potential values:
+         *    "NOT ADOPTED"
+         *    "ADOPTED"
+         */
+        [Required]
+        [StringLength(150, MinimumLength = 4)]
+        public string? DogStatus { get; set; }
     }
 }
